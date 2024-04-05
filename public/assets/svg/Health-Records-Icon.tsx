ca77acde7fa780ab1f76ce92877a60428add1f07
“Abdullah-Abuzaid” <bozaida@gmail.com>
2024-04-06 02:00:54 +0300

    icons

---
+++ public/assets/svg/Health-Records-Icon.tsx
@@ -0,0 +1,30 @@
+export default function HeathRecordsIcon() {
+  return (
+    <svg
+      width="38"
+      height="32"
+      viewBox="0 0 38 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M31.86 31H6.14C3.32 31 1 28.666 1 25.829V6.17101C1 3.33399 3.32 1 6.14 1H31.86C34.68 1 37 3.33399 37 6.17101V25.829C37 28.666 34.68 31 31.86 31Z"
+        stroke="white"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M1 15.9899H6.64L9.89999 13.0322L12.08 18.4849L15.96 3.81689L17.24 26.5131L21.82 12.831L24.84 19.2294L26.46 15.9899H31.58"
+        stroke="white"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M31.5798 17.8209C32.5849 17.8209 33.3998 17.0012 33.3998 15.9899C33.3998 14.9787 32.5849 14.1589 31.5798 14.1589C30.5746 14.1589 29.7598 14.9787 29.7598 15.9899C29.7598 17.0012 30.5746 17.8209 31.5798 17.8209Z"
+        fill="#0079AB"
+      />
+    </svg>
+  );
+}
