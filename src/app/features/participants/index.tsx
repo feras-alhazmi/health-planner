e71ee2ca1d4f2c17d5b905cba6300b4807c49c15
junaid786-prog <junaidshoaib555@gmail.com>
2024-04-17 09:35:29 +0500

    new file

---
+++ src/app/features/participants/index.tsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/features/Navbar"
+import ParticipantsCatalog from "./components/features/ParticipantsCatalog"
+import Sidebar from "./components/features/Sidebar"
+
+const Page = () => {
+    return (
+        <main className='flex w-full h-[100vh]'>
+            <div className='h-full hidden md:block'>
+                <Sidebar />
+            </div>
+            <div className='flex flex-col flex-1 h-full bg-gray-200 p-10'>
+                <Navbar />
+                <section className='flex mt-10 flex-grow flex-1'>
+                    <ParticipantsCatalog />
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default Page
